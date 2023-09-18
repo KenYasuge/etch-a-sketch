e5e8bf0f7f5a0c98433976aed3ffd9b6cdc7165d
Chiken <24449663+KenYasuge@users.noreply.github.com>
2023-09-18 11:53:02 +0800

    Finished algorithm for making boxes that fit perfectly.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function createDivs(size){
-    // create x rows, fill with x numbers
+// Default size = 16px
+makeSketch(16);
+
+function makeSketch(size){
+    // Create x rows, fill with x numbers
     for (let i = 0; i < size; i++){
         const boxesContainer = document.createElement('div');
         boxesContainer.setAttribute('class', 'sketch-boxes-container');
@@ -8,6 +11,10 @@ function createDivs(size){
         for (let i = 0; i < size; i++){
             const boxes = document.createElement('div');
             boxes.setAttribute('class', 'sketch-boxes');
+            // Make each box size fill the 500px space
+            let boxSize = 500 / size + 'px';
+            boxes.style.width = boxSize;
+            boxes.style.height = boxSize;
             boxesContainer.appendChild(boxes);
         }
     }
